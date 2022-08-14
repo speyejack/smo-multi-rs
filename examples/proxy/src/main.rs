5864cf2b0e0d729fa0874f630b7c479b4d902bd3
Jack Garrard <speyejack@gmail.com>
2022-08-14 15:09:17 -0700

    Made proxy for demoing udp

--- examples/proxy/src/main.rs
+++ examples/proxy/src/main.rs
@@ -5,6 +5,7 @@ use smoo::net::connection::Connection;
 use smoo::net::{encoding::Encodable, Packet, PacketData};
 use smoo::types::Result;
 use std::ops::Not;
+use std::time::Instant;
 use std::{io::Cursor, net::SocketAddr};
 use tokio;
 use tokio::net::UdpSocket;
@@ -13,6 +14,9 @@ use tracing::Instrument;
 use tracing_subscriber::EnvFilter;
 use udp_conn::UdpConnection;
 
+type LocalAddrs = (SocketAddr, SocketAddr);
+type RemoteAddrs = (SocketAddr, SocketAddr, Origin);
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt()
@@ -20,19 +24,55 @@ async fn main() -> Result<()> {
         .init();
 
     tracing::debug!("Starting");
-    let addr: SocketAddr = "0.0.0.0:61884".parse().unwrap();
+
+    let mut args = std::env::args();
+    let is_client = args.nth(1).unwrap() == "client";
+    let (span, local_bind, remote_addrs) = if is_client {
+        // Client side
+        tracing::info!("Client side proxy");
+        let local_bind: LocalAddrs = (
+            "0.0.0.0:1027".parse().unwrap(),  // TCP
+            "0.0.0.0:55446".parse().unwrap(), // UDP
+        );
+        let remote_addrs: RemoteAddrs = (
+            "192.168.1.39:1028".parse().unwrap(),
+            "192.168.1.39:55445".parse().unwrap(),
+            // "127.0.0.1:61885".parse().unwrap(),
+            // "127.0.0.1:55445".parse().unwrap(),
+            Origin::Server,
+        );
+        (tracing::info_span!("client"), local_bind, remote_addrs)
+    } else {
+        // Server side
+        tracing::info!("Server side proxy");
+        let local_bind: LocalAddrs = (
+            "0.0.0.0:61885".parse().unwrap(), // TCP
+            "0.0.0.0:55445".parse().unwrap(), // UDP
+        );
+        let remote_addrs: RemoteAddrs = (
+            "64.201.219.20:1027".parse().unwrap(), // Server address
+            // "127.0.0.1:61888".parse().unwrap(),
+            // "127.0.0.1:55446".parse().unwrap(),
+            "192.168.1.39:55446".parse().unwrap(),
+            Origin::Client,
+        );
+        (tracing::info_span!("server"), local_bind, remote_addrs)
+    };
+    let _span = span.enter();
+
+    let addr: SocketAddr = local_bind.0;
     let listener = TcpListener::bind(addr).await?;
 
     loop {
         let (from_socket, addr) = listener.accept().await?;
-        let udp = UdpSocket::bind("0.0.0.0:55443").await.unwrap();
-        let direction = Origin::Client;
+        let udp = UdpSocket::bind(local_bind.1).await.unwrap();
         tracing::info!("new client connection: {}", addr);
         let span = tracing::info_span!("cli", addr = addr.ip().to_string());
 
+        let remote_addrs = remote_addrs.clone();
         tokio::spawn(
             async move {
-                let result = proxy_client(from_socket, udp, direction).await;
+                let result = proxy_client(from_socket, udp, remote_addrs).await;
                 if let Err(e) = result {
                     tracing::warn!("Client error: {e}");
                 }
@@ -60,23 +100,32 @@ impl Not for Origin {
     }
 }
 
-async fn proxy_client(cli_sock: TcpStream, udp_sock: UdpSocket, plex: Origin) -> Result<()> {
+async fn proxy_client(
+    cli_sock: TcpStream,
+    udp_sock: UdpSocket,
+    to_addrs: RemoteAddrs,
+) -> Result<()> {
     let server_conn = tokio::net::TcpSocket::new_v4()?;
-    let addr = "192.168.1.39:1027".parse().unwrap();
+    let addr = to_addrs.0;
+    let udp_addr = to_addrs.1;
+    let plex = to_addrs.2;
     let serv_sock = server_conn.connect(addr).await?;
+    // udp_sock.connect(udp_addr).await.unwrap();
 
     let mut cli = Connection::new(cli_sock);
     let mut serv = Connection::new(serv_sock);
-    let mut udp = UdpConnection::new(udp_sock);
+    let mut udp = UdpConnection::new(udp_sock, udp_addr);
+    let mut use_udp = true;
+    let mut last_tag_packet = Instant::now();
 
     tracing::info!("Client setup and ready");
     loop {
         let (origin, packet_result) = tokio::select! {
-            packet_r = udp.read_packet() => {(!plex, packet_r)}
+            packet_r = udp.read_packet() => {(plex, packet_r)}
             packet_r = cli.read_packet() => {(Origin::Client, packet_r)},
             packet_r = serv.read_packet() => {(Origin::Server, packet_r)},
         };
-        tracing::debug!("packet: {:?}", &packet_result);
+        tracing::trace!("packet: {:?}", &packet_result);
 
         let mut packet = packet_result?;
         packet.resize();
@@ -86,24 +135,29 @@ async fn proxy_client(cli_sock: TcpStream, udp_sock: UdpSocket, plex: Origin) ->
             Origin::Server => (&mut serv, &mut cli),
         };
 
-        if origin == plex {
+        tracing::debug!("got packet: {}", packet.data.get_type_name());
+        if let &Packet {
+            data: PacketData::Tag { .. },
+            ..
+        } = &packet
+        {
+            if last_tag_packet.elapsed().as_millis() < 1000 {
+                use_udp = !use_udp;
+                tracing::info!("Using udp: {}", use_udp);
+            }
+            last_tag_packet = Instant::now();
+        };
+
+        if use_udp && origin != plex {
             if let &Packet {
-                id: _,
-                data_size: _,
                 data: PacketData::Player { .. },
+                ..
             } = &packet
             {
-                udp.write_packet(&packet).await?;
+                udp.write_packet(&packet).await.unwrap();
                 continue;
             }
         }
-
-        // let mut buff = BytesMut::with_capacity(300);
-        // packet.encode(&mut buff);
-        // assert_eq!(&current_conn.last_data[..].len(), &buff[..].len());
-
-        tracing::info!("got packet: {}", packet.data.get_type_name());
-
         dest_conn.write_packet(&packet).await?
     }
 }
--- examples/proxy/src/udp_conn.rs
+++ examples/proxy/src/udp_conn.rs
@@ -1,6 +1,6 @@
 use std::{io::Cursor, net::SocketAddr};
 
-use bytes::{Buf, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt, BufWriter},
     net::{TcpStream, UdpSocket},
@@ -14,16 +14,16 @@ use smoo::{
 #[derive(Debug)]
 pub struct UdpConnection {
     pub socket: UdpSocket,
-    pub addr: SocketAddr,
     pub buff: BytesMut,
+    pub send_addr: SocketAddr,
 }
 
 impl UdpConnection {
-    pub fn new(stream: UdpSocket) -> Self {
+    pub fn new(stream: UdpSocket, addr: SocketAddr) -> Self {
         UdpConnection {
-            addr: stream.peer_addr().unwrap(),
             socket: stream,
             buff: BytesMut::with_capacity(1024),
+            send_addr: addr,
         }
     }
 
@@ -55,20 +55,21 @@ impl UdpConnection {
     }
 
     pub async fn read_socket(&mut self) -> Result<()> {
-        let _read_amount = self.socket.recv(&mut self.buff).await?;
+        let mut buff = vec![0u8; 100];
+        let read_amount = self.socket.recv_from(&mut buff).await?;
+        self.buff.put_slice(&buff[..read_amount.0]);
         Ok(())
     }
 
     pub async fn write_packet(&mut self, packet: &Packet) -> Result<()> {
         let mut buff = BytesMut::with_capacity(MAX_PACKET_SIZE);
         packet.encode(&mut buff)?;
-        tracing::trace!("Writing packet: {:?}", packet);
+
         let mut amount = 0;
         while amount < buff.len() {
-            let last_write = self.socket.send(&buff[..]).await?;
-            amount += last_write;
+            let last_write = self.socket.send_to(&buff[..], self.send_addr).await;
+            amount += last_write.unwrap();
         }
-        tracing::trace!("Packet written");
         Ok(())
     }
 }
