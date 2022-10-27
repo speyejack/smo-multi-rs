c2dd19f6d6c9040454ea1fa278ceaacc4bc71646
Jack Garrard <speyejack@gmail.com>
2022-10-27 05:03:09 -0700

    Packet names in wireshark

--- smo-wireshark.lua
+++ smo-wireshark.lua
@@ -5,9 +5,12 @@ function smo_proto.dissector(buffer, pinfo, tree)
    pinfo.cols.protocol = "SMOO"
    local subtree = tree:add(smo_proto, buffer(), "SMO Protocol Data")
    local type_id = buffer(16,2):le_uint()
-   subtree:add_le(buffer(0,0), "Protocol: " .. proto)
+   local type_name = "Unknown packet"
+   if type_id > 0 and type_id < 15 then
+	  type_name = packet_names[type_id]
+   end
    subtree:add(buffer(0,16), "The UID: " .. buffer(0,16))
-   subtree:add_le(buffer(16,2), "Type id: " .. type_id)
+   subtree:add_le(buffer(16,2), "Packet type: " .. type_name .. "(" .. type_id .. ")")
    subtree:add_le(buffer(18,2), "Data size: " .. buffer(18,2):le_uint())
    if type_id == 2 then
 	  subtree:add_le(buffer(20,4), "Vec x: " .. buffer(20,4):le_float())
@@ -19,6 +22,7 @@ function smo_proto.dissector(buffer, pinfo, tree)
    end
 end
 
+packet_names = {"Init", "Player", "Cap", "Game", "Tag", "Connect", "Disconnect", "Costume", "Shine", "Capture", "ChangeStage", "Command", "UdpInit", "HolePunch"}
 
 tcp_table = DissectorTable.get("tcp.port")
 tcp_table:add(1027, smo_proto)
